44694fc7496834791dacda1d1417dc9abc789523
Mohd Rohaan <rohaanmohd2000@gmail.com>
2026-03-14 23:29:30 +0530

    kafi ho gaya

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -19,4 +19,16 @@ api.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+// Global error handler
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
